c0bddca37010b7dba665ff7a376c1313b4b0b9e9
Paul Charles <santosh41690@gmail.com>
2017-10-10 00:08:50 -0500

    Upload Code

---
+++ Lab7/Source/mainApp/www/tests/unit_tests.js
@@ -0,0 +1,91 @@
+//Test 1
+describe("Verify if length of a loop ends", function() {
+  var num;
+    var val;
+
+  beforeEach(function() {
+    val >= 0;
+  });
+
+  afterEach(function() {
+    val > 0;
+  });
+
+  it("is accurate and ends with length", function() {
+    expect(val).toEqual(num);
+  });
+
+});
+
+//Test 2
+describe("Verify if String is Same as URL", function() {
+  var string;
+    var url;
+
+  beforeEach(function() {
+    url != null;
+  });
+
+ 
+  it("is accurate and ends with string", function() {
+    expect(url).toEqual(string);
+  });
+
+});
+
+//Test 3
+describe("Validate that Google Search and Foursquare gives 5 values", function() {
+  var googleCount;
+    var foursquareeCount;
+    var googleString;
+    var foursquareeString;
+
+    var val;
+  beforeEach(function() {
+    if(foursquareeString != null)
+        {
+            return false;
+        }
+      if(googleString != null)
+        {
+            return false;
+        }
+  });
+
+ if(val)
+     {
+          it("is accurate and ends with string", function() {
+            expect(googleCount).toEqual(5);
+              expect(foursquareeCount).toEqual(5);
+          });
+     }
+ 
+
+});
+
+//Test 4
+ describe("Validate Google and Foursquare Strings", function() {
+          
+        var googleCount;
+    var foursquareeCount;
+    var googleString;
+    var foursquareeString;
+
+     var aString;
+     var bString;
+    it("Make sure it is equal to 5", function() {
+      var a = 5;
+      expect(a).toEqual(5);
+    });
+
+    it("Validate numbers and strings", function() {
+      var foo = {
+       foursquareeCount: 5,
+        foursquareeString: aString
+      };
+      var googleSearch = {
+        googleCount: 5,
+        googleString: bString
+      };
+    });
+  });
