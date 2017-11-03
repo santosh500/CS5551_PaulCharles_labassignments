70935c8838415f971c7ed7f485d0c613c14c33cb
Paul Charles <santosh41690@gmail.com>
2017-11-03 01:29:45 -0500

    Add more code

---
+++ Lab10/Source/Lab10_NodeJS_Heroku/MongoConnection.js
@@ -0,0 +1,15 @@
+/**
+ * Created by mnpw3d on 20/10/2016.
+ */
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://root:secure@ds161483.mlab.com:61483/asefall17';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
+
+/*
+url :   mongodb://root:secure@ds161483.mlab.com:61483/asefall17
+ */
---
+++ Lab10/Source/Lab10_NodeJS_Heroku/app.js
@@ -0,0 +1,24 @@
+//This Code and Project Template is referenced and derived from: https://github.com/mschwarzmueller/nodejs-basics-tutorial
+//This file defines the locations and paths of the application
+var logs = require('morgan');
+var path = require('path');
+var exp = require('express');
+var bodyParser = require('body-parser');
+var hbs = require('express-handlebars');
+var routes = require('./index');
+
+var app = exp();
+
+
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(exp.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+
+module.exports = app;
